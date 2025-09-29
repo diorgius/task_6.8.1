7c1f7839364e073420977d7679c54def32e384a1
diorgius <diorgius@gmail.com>
2025-09-29 18:21:42 +0300

    update task3

--- task3/script.js
+++ task3/script.js
@@ -19,3 +19,47 @@ function makeRed() {
     trafficLightEl.removeEventListener('click', makeRed);
     trafficLightEl.addEventListener('click', makeGreen);
 }
+
+const trafficLightGreen = document.querySelector('#trafficLightGreen');
+const trafficLightYellow = document.querySelector('#trafficLightYellow');
+const trafficLightRed = document.querySelector('#trafficLightRed');
+
+function makeGreenCircle() {
+    trafficLightGreen.style.background = ('green');
+    trafficLightYellow.style.background = ('black');
+    trafficLightRed.style.background = ('black');    
+    trafficLightGreen.removeEventListener('click', makeGreenCircle);
+    trafficLightYellow.removeEventListener('click', makeGreenCircle);
+    trafficLightRed.removeEventListener('click', makeGreenCircle);
+    trafficLightGreen.addEventListener('click', makeYellowCircle);
+    trafficLightYellow.addEventListener('click', makeYellowCircle);
+    trafficLightRed.addEventListener('click', makeYellowCircle);
+}
+
+function makeYellowCircle() {
+    trafficLightYellow.style.background = ('yellow');
+    trafficLightGreen.style.background = ('black');
+    trafficLightRed.style.background = ('black');    
+    trafficLightYellow.removeEventListener('click', makeYellowCircle);
+    trafficLightGreen.removeEventListener('click', makeYellowCircle);
+    trafficLightRed.removeEventListener('click', makeYellowCircle);
+    trafficLightYellow.addEventListener('click', makeRedCircle);
+    trafficLightGreen.addEventListener('click', makeRedCircle);
+    trafficLightRed.addEventListener('click', makeRedCircle);
+}
+
+function makeRedCircle() {
+    trafficLightRed.style.background = ('red');
+    trafficLightGreen.style.background = ('black');
+    trafficLightYellow.style.background = ('black');    
+    trafficLightRed.removeEventListener('click', makeRedCircle);
+    trafficLightGreen.removeEventListener('click', makeRedCircle);
+    trafficLightYellow.removeEventListener('click', makeRedCircle);
+    trafficLightRed.addEventListener('click', makeGreenCircle);
+    trafficLightGreen.addEventListener('click', makeGreenCircle);
+    trafficLightYellow.addEventListener('click', makeGreenCircle);
+}
+
+trafficLightGreen.addEventListener('click', makeGreenCircle);
+trafficLightYellow.addEventListener('click', makeYellowCircle);
+trafficLightRed.addEventListener('click', makeRedCircle);
