25a966c5f0a1396e2d04071a06c652c8bc5c0656
diorgius <diorgius@gmail.com>
2025-09-29 16:44:27 +0300

    add task5

---
+++ task5/script.js
@@ -0,0 +1,15 @@
+let textField = document.querySelector('#textField');
+let duplicateField = document.querySelector('#duplicateField');
+
+textField.addEventListener('keydown', (Event) => {
+    duplicateField.textContent = textField.value;
+});
+
+const clearButton = document.querySelector('#clearButton');
+
+clearButton.addEventListener('click', (Event) => {
+    Event.preventDefault();
+    console.log(textField.value);
+    textField.value = "";
+    duplicateField.textContent = "";
+});
